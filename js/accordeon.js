97edf8623a1d20c5f754a0a6f42f2fa1b71db70e
Aell-j <asulejmanova53@gmail.com>
2025-11-13 09:44:41 +0300

    Добавлена отправка данных на севрер

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,15 +1,19 @@
-const body = document.querySelector("body")
-const contents = document.querySelectorAll(".program-line_content")
-const allDescr = document.querySelectorAll(".program-line_descr")
+const accordeon = () => {
+  const body = document.querySelector("body")
+  const contents = document.querySelectorAll(".program-line_content")
+  const allDescr = document.querySelectorAll(".program-line_descr")
 
-contents.forEach((e) => {
-  const title = e.querySelector(".program-line_title")
-  const descr = e.querySelector(".program-line_descr")
+  contents.forEach((e) => {
+    const title = e.querySelector(".program-line_title")
+    const descr = e.querySelector(".program-line_descr")
 
-  title.addEventListener("click", () => {
-    allDescr.forEach((e) => {
-      e.classList.remove("active")
+    title.addEventListener("click", () => {
+      allDescr.forEach((e) => {
+        e.classList.remove("active")
+      })
+      descr.classList.add("active")
     })
-    descr.classList.add("active")
   })
-})
+}
+
+accordeon()
